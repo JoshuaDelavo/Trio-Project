4f253fa6e68eb5f1f67af6678ef5805fa686b845
MhdAnggieRinovka <anggierinovka@gmail.com>
2021-05-31 17:27:01 +0700

    isi karousel

--- src/components/ClientCarousel/index.js
+++ src/components/ClientCarousel/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 import { CarouselContainer, FirstCaption, ClientLogo, SecondCaption, Button, ImageBackgroundContainer, ImageBackground, ImageCrop, ImageCrop2, ColCustom, RowCustom } from './CarouselElement'
 import HavillaLogo from '../../images/havilla_logo.svg'
 import HavillaLaptop from '../../images/havilla_laptop.png'
@@ -7,17 +7,28 @@ import Flickity from 'react-flickity-component'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { Element } from 'react-scroll'
-
+import CelebritiesApi from '../../config/CelebritiesApi';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { baseURL } from '../../config/';
 const ClientCarousel = () => {
+    const [celeb, setCeleb] = useState({ photo: [''] });
+    useEffect(() => {
+        CelebritiesApi.find().then(res => {
+            setCeleb(res[0]);
+            console.log(celeb);
+
+        })},[])
     return (
         <Element id='works' name='works'>
             <Carousel>
+            
                 <CarouselContainer>
-                    <FirstCaption>POWERED BY ANTHEM</FirstCaption>
                     <ImageCrop>
                     </ImageCrop>
                     <ImageCrop2>
                     </ImageCrop2>
+                    <FirstCaption>POWERED BY ANTHEM</FirstCaption>
                     <RowCustom>
                         <ColCustom xs={12} lg={4} lg={{ order: 'first' }}>
                             <ClientLogo src={HavillaLogo} style={{ width: '300px' }}></ClientLogo>
@@ -27,7 +38,7 @@ const ClientCarousel = () => {
                             </Button>
                         </ColCustom>
                         <Col xs={12} lg={8} xs={{ order: 'first' }}>
-                            <ImageBackground src={HavillaLaptop} className="d-block mx-auto img-fluid"></ImageBackground>
+                            <ImageBackground src={ `url("${baseURL}${celeb.photo[0].url}")`} className="d-block mx-auto img-fluid"></ImageBackground>
                         </Col>
                     </RowCustom>
                 </CarouselContainer>
