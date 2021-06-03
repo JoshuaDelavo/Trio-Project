d50f4ae010453c92c9e4e59d24871bd83a8f441b
MhdAnggieRinovka <anggierinovka@gmail.com>
2021-06-03 21:39:02 +0700

    nambah multi-carousel

--- src/components/ClientCarousel/index.js
+++ src/components/ClientCarousel/index.js
@@ -4,13 +4,15 @@ import HavillaLogo from '../../images/havilla_logo.svg'
 import HavillaLaptop from '../../images/havilla_laptop.png'
 import { Col } from 'react-bootstrap';
 import Flickity from 'react-flickity-component'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import { Carousel } from 'react-responsive-carousel';
 import { Element } from 'react-scroll'
 import CelebritiesApi from '../../config/CelebritiesApi';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { baseURL } from '../../config/';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const ClientCarousel = () => {
     const [celeb, setCeleb] = useState([]);
     useEffect(() => {
@@ -19,59 +21,45 @@ const ClientCarousel = () => {
             console.log(celeb);
         })
     }, [])
+
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+      
+
     return (
         <Element id='works' name='works'>
-            <Carousel>
+            <Carousel responsive={responsive}>
 
 
 
                 {
                     celeb.map((cek, i) => {
                         return (
-                            <CarouselContainer>
-                                <ImageCrop>
-                                </ImageCrop>
-                                <ImageCrop2>
-                                </ImageCrop2>
-                                <FirstCaption>POWERED BY ANTHEM</FirstCaption>
-                                <RowCustom>
-                                    <ColCustom xs={12} lg={4} lg={{ order: 'first' }}>
-                                        <ClientLogo src={HavillaLogo} style={{ width: '300px' }}></ClientLogo>
-                                        <SecondCaption>Create Your <br /> Own Tea Blend,<br /> Digitally</SecondCaption>
-                                        <Button to='/story'>
-                                            Read Stories
-                                        </Button>
-                                    </ColCustom>
+                                
                                     <Col xs={12} lg={8} xs={{ order: 'first' }}>
                                         <ImageBackground src={baseURL + (cek.photo[0].url)} className="d-block mx-auto img-fluid"></ImageBackground>
                                     </Col>
-                                </RowCustom>
-                            </CarouselContainer>
 
                         )
                     })
                 }
 
 
-                <CarouselContainer>
-                    <ImageCrop>
-                    </ImageCrop>
-                    <ImageCrop2>
-                    </ImageCrop2>
-                    <FirstCaption>POWERED BY ANTHEM</FirstCaption>
-                    <RowCustom>
-                        <ColCustom xs={12} lg={4} lg={{ order: 'first' }}>
-                            <ClientLogo src={HavillaLogo} style={{ width: '300px' }}></ClientLogo>
-                            <SecondCaption>Create Your <br /> Own Tea Blend,<br /> Digitally</SecondCaption>
-                            <Button to='/story'>
-                                Read Stories
-                            </Button>
-                        </ColCustom>
-                        <Col xs={12} lg={8} xs={{ order: 'first' }}>
-                            <ImageBackground src={HavillaLaptop} className="d-block mx-auto img-fluid"></ImageBackground>
-                        </Col>
-                    </RowCustom>
-                </CarouselContainer>
             </Carousel>
         </Element >
     )
