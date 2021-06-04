8becacd3f4751407cd3f9bf7f168975af9dfe500
obolsuro <obolsuro@gmail.com>
2021-06-04 09:45:26 +0700

    Cross
    ganti cross

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Nav, NavBurgerIcon, SidebarMenu, SidebarItem, SidebarLinkRoute } from './SidebarElement'
 import { FaTimes, FaBars } from 'react-icons/fa'
 import Scroll from 'react-scroll'
+import { GrClose } from "react-icons/gr";
 const ScrollLink = Scroll.Link
 
 const Sidebar = ({ toggle, hamburgerOpen }) => {
     return (
         <Nav hamburgerOpen={hamburgerOpen} onClick={toggle}>
             <NavBurgerIcon onClick={toggle}>
-                {hamburgerOpen ? <FaTimes onClick={toggle}></FaTimes> : ''} CLOSE
+                {hamburgerOpen ? <GrClose onClick={toggle}></GrClose> : ''} CLOSE
             </NavBurgerIcon>
             <SidebarMenu>
                 <SidebarItem>
