b55594cc7c40daa4b837d3ad506506c771291300
Joshua Delavo <jdelavo@gmail.com>
2021-06-04 21:19:09 +0700

    buang spasi

--- src/components/ClientCarousel/index.js
+++ src/components/ClientCarousel/index.js
@@ -60,7 +60,6 @@ const ClientCarousel = () => {
                 return (
                   <div className={i === imageIndex ? "slide activeSlide" : "slide"}>
                   <img src={baseURL + (cek.photo[0].url)} ></img>
-                  <br></br>
                   <SectionParagraph value={cek.name}></SectionParagraph>
                   </div>
                 )
