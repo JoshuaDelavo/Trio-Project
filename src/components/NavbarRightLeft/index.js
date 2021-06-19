6d9423ad6243295d6984377aca5aa314f3001b88
obolsuro <obolsuro@gmail.com>
2021-06-20 01:27:57 +0700

    Cek JOSH
    testis

--- src/components/NavbarRightLeft/index.js
+++ src/components/NavbarRightLeft/index.js
@@ -63,10 +63,12 @@ const NavbarRightLeft = ({ toggle, toggle2, hamburgerOpen, conciergeOpen }) => {
           <NavLogo to="/">
             <img src={baseURL + logo.logo.url} alt=""></img>
           </NavLogo>
-          <NavTulisan onClick={toggle2}>
-            THE CONCIERGE{" "}
-            {conciergeOpen ? "" : <BiEditAlt onClick={toggle2}></BiEditAlt>}
-          </NavTulisan>
+          <NavBurgerIcon>
+            <NavTulisan onClick={toggle2}>
+              THE CONCIERGE{" "}
+              {conciergeOpen ? "" : <BiEditAlt onClick={toggle2}></BiEditAlt>}
+            </NavTulisan>
+          </NavBurgerIcon>
           <NavEditIcon onClick={toggle2}>
             {conciergeOpen ? "" : <BiEditAlt onClick={toggle2}></BiEditAlt>}
           </NavEditIcon>
