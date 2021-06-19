9ff19b860645659bff110134e5ce422d4dcacf41
MhdAnggieRinovka <anggierinovka@gmail.com>
2021-06-19 17:29:10 +0700

    copy link

--- src/sections/NewsDetail/index.js
+++ src/sections/NewsDetail/index.js
@@ -11,7 +11,22 @@ import { GrTwitter } from "react-icons/gr";
 import { RiFacebookCircleFill } from "react-icons/ri";
 import { IoMdCopy } from "react-icons/io";
 import { FacebookShareButton, TwitterShareButton } from "react-share";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core/styles";
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 const NewsDetails = () => {
   const [detail, setDetail] = useState([]);
   const [celebrities, setCelebrities] = useState([]);
@@ -31,6 +46,20 @@ const NewsDetails = () => {
     document.body.removeChild(el);
     alert("URL Copied");
   }
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   let iconStyles = { color: "white", fontSize: "1.5em", marginLeft: "1em" };
   let arrow = { color: "white", fontSize: "1.5em", marginRight: "0.5em" };
@@ -70,7 +99,12 @@ const NewsDetails = () => {
               <RiFacebookCircleFill style={iconStyles}></RiFacebookCircleFill>
             </FacebookShareButton>
 
-            <IoMdCopy style={iconStyles} onClick={copy}></IoMdCopy>
+            <IoMdCopy style={iconStyles} onClick={handleClick}></IoMdCopy>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+              <Alert onClose={handleClose} severity="success">
+                {"" + window.location.href}
+              </Alert>
+            </Snackbar>
           </div>
         </div>
         <br />
