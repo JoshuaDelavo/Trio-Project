3771043acf370f224f72473e32a17c7d93fa7215
obolsuro <obolsuro@gmail.com>
2021-07-02 00:58:01 +0700

    Make function for hide show
    test

--- src/components/Concierge/page3.js
+++ src/components/Concierge/page3.js
@@ -22,6 +22,7 @@ import {
 } from '@material-ui/pickers';
 import { NavCon, NavBurgerIcon, ConMenu, ConItem, ButtonText, BottomIcon } from './ConciergeElements'
 import { useStyles } from '@material-ui/pickers/views/Calendar/SlideTransition';
+import dateFormat from 'dateformat';
 
 
 
@@ -29,23 +30,38 @@ class Page3 extends Component {
     constructor() {
         super();
         this.state = {
-            name: "React",
-            nama: '',
-            age: '',
-            color: '',
-            size_country: '',
-            size: '',
-            city: '',
-            country: '',
-            date: '',
-            photo: '',
-            budget: '',
-            email: '',
-            phone_code: '',
-            number: '',
-            now: 1
+            data: {
+                name: "React",
+                nama: '',
+                age: '',
+                color: '',
+                sizes: '',
+                code_size: '',
+                city: '',
+                country: '',
+                datess: new Date(),
+                photo: '',
+                budget: '',
+                email: '',
+                phone_code: '',
+                number: '',
+            }
         };
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeAge = this.handleChangeAge.bind(this);
         this.handleChangeColor = this.handleChangeColor.bind(this);
+        this.handleChangeSize = this.handleChangeSize.bind(this);
+        this.handleChangeCode = this.handleChangeCode.bind(this);
+        this.handleChangeCity = this.handleChangeCity.bind(this);
+        this.handleChangeCountries = this.handleChangeCountries.bind(this);
+        this.handleChangeDate = this.handleChangeDate.bind(this);
+        this.handleChangeFile = this.handleChangeFile.bind(this);
+        this.handleChangeBudget = this.handleChangeBudget.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePhoneCode = this.handleChangePhoneCode.bind(this);
+        this.handleChangeNumber = this.handleChangeNumber.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
     }
 
     useStyles1 = makeStyles((theme) => ({
@@ -69,24 +85,94 @@ class Page3 extends Component {
         },
     }));
 
+    handleChangeName(event) {
+        this.setState({ nama: event.target.value })
+        document.getElementById(1).className = "show"
+        console.log(this.state.nama)
+    };
+    handleChangeAge(event) {
+        this.setState({ age: event.target.value })
+        document.getElementById(2).className = "show"
+        console.log(this.state.age)
+    };
     handleChangeColor(event) {
-        this.setState({ name: event.target.value })
-        document.getElementById(this.state.now).className = "show"
-        console.log(this.state.name)
+        this.setState({ color: event.target.value })
+        document.getElementById(3).className = "show"
+        console.log(this.state.color)
+    };
+    handleChangeSize(event) {
+        this.setState({ sizes: event.target.value })
+        console.log(this.state.sizes)
+    };
+    handleChangeCode(event) {
+        this.setState({ code_size: event.target.value })
+        document.getElementById(4).className = "show"
+        console.log(this.state.code_size)
+    };
+    handleChangeCity(event) {
+        this.setState({ city: event.target.value })
+        console.log(this.state.city)
+    };
+    handleChangeCountries(event) {
+        this.setState({ country: event.target.value })
+        document.getElementById(5).className = "show"
+        console.log(this.state.country)
+    };
+    handleChangeDate(date) {
+        var test = dateFormat(date, "MM/dd/yyyy")
+
+        this.setState({ datess: date })
+        document.getElementById(6).className = "show"
+        document.getElementById(7).className = "show"
+        console.log(this.state.datess)
+    };
+    handleChangeFile(file, cb) {
+        document.getElementById(6).className = "show"
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            cb(reader.result)
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+        console.log(reader.result);
+    };
+    handleChangeBudget(event) {
+        this.setState({ budget: event.target.value })
+        document.getElementById(8).className = "show"
+        console.log(this.state.budget)
+    };
+    handleChangeEmail(event) {
+        this.setState({ email: event.target.value })
+        document.getElementById(9).className = "show"
+        console.log(this.state.email)
+    };
+    handleChangePhoneCode(event) {
+        this.setState({ phone_code: event.target.value })
+        console.log(this.state.phone_code)
+    };
+    handleChangeNumber(event) {
+        this.setState({ number: event.target.value })
+        document.getElementById(10).className = "show"
+        console.log(this.state.number)
+    };
+    handleSubmit() {
+        console.log(this.state.datess, this.state.sizes)
     };
 
     render() {
         return <div>
             <div id='0'>
                 My Name is
-                <TextField onChange={this.handleChangeColor} id="filled-basic" label="Name" variant="filled" />
+                <TextField onChange={this.handleChangeName} id="filled-basic" label="Name" variant="filled" />
             </div>
-            <div id='1' className="hide">
+            <div id='1' className="hide" >
                 and I'm
-                <TextField id="filled-basic" label="Name" variant="filled" />
+                <TextField type="number" onChange={this.handleChangeAge} id="filled-basic" label="Name" variant="filled" />
                 years old
             </div>
-            <div id='2'>
+            <div id='2' className="hide">
                 My favourite color is
                 <FormControl className={this.useStyles1.formControl}>
                     <Select
@@ -94,6 +180,7 @@ class Page3 extends Component {
                         displayEmpty
                         className={this.useStyles.selectEmpty}
                         inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeColor}
                     >
                         <MenuItem value="" disabled>
                             Color
@@ -104,27 +191,14 @@ class Page3 extends Component {
                     </Select>
                 </FormControl>
             </div>
-            <div id='2'>
+            <div id='3' className="hide">
                 and my dress size is
                 <FormControl className={this.useStyles1.formControl}>
                     <Select
                         displayEmpty
                         className={this.useStyles.selectEmpty}
                         inputProps={{ 'aria-label': 'Without label' }}
-                    >
-                        <MenuItem value="" disabled>
-                            code
-                        </MenuItem>
-                        <MenuItem value={'Euro'}>EU</MenuItem>
-                        <MenuItem value={'US'}>US</MenuItem>
-                        <MenuItem value={'JPN'}>JPN</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl className={this.useStyles1.formControl}>
-                    <Select
-                        displayEmpty
-                        className={this.useStyles.selectEmpty}
-                        inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeSize}
                     >
                         <MenuItem value="" disabled>
                             Size
@@ -134,14 +208,30 @@ class Page3 extends Component {
                         <MenuItem value={'3'}>3</MenuItem>
                     </Select>
                 </FormControl>
+                <FormControl className={this.useStyles1.formControl}>
+                    <Select
+                        displayEmpty
+                        className={this.useStyles.selectEmpty}
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeCode}
+                    >
+                        <MenuItem value="" disabled>
+                            code
+                        </MenuItem>
+                        <MenuItem value={'Euro'}>EU</MenuItem>
+                        <MenuItem value={'US'}>US</MenuItem>
+                        <MenuItem value={'JPN'}>JPN</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
-            <div id='2'>
+            <div id='4' className="hide">
                 I live in
                 <FormControl className={this.useStyles1.formControl}>
                     <Select
                         displayEmpty
                         className={this.useStyles.selectEmpty}
                         inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeCity}
                     >
                         <MenuItem value="" disabled>
                             City
@@ -151,16 +241,33 @@ class Page3 extends Component {
                         <MenuItem value={'TaiPei'}>Tai Pei</MenuItem>
                     </Select>
                 </FormControl>
-                <Countries>
-                </Countries>
+                <FormControl className={this.useStyles1.formControl}>
+                    <Select
+                        displayEmpty
+                        className={this.useStyles.selectEmpty}
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeCountries}
+                    >
+                        <MenuItem value="" disabled>
+                            Countries
+                        </MenuItem>
+                        <MenuItem value={'Indonesia'}>Indonesia</MenuItem>
+                        <MenuItem value={'China'}>China</MenuItem>
+                        <MenuItem value={'United State'}>United State</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
-            <div id='2'>
+            <div id='5' className="hide">
                 and I need to use this dress at
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <MuiPickersUtilsProvider
+                    onChange={this.handleChangeDate}
+                    utils={DateFnsUtils}>
                     <KeyboardDatePicker
                         disableToolbar
                         variant="inline"
                         format="MM/dd/yyyy"
+                        value={this.state.datess}
+                        onChange={this.handleChangeDate}
                         margin="normal"
                         id="date-picker-inline"
                         label="Date picker inline"
@@ -170,17 +277,18 @@ class Page3 extends Component {
                     />
                 </MuiPickersUtilsProvider>
             </div>
-            <div id='2'>
+            <div id='6' className="hide">
                 I have preferance for my dress
                 <input type="file" accept="image/png, image/jpeg,file/pdf"></input>
             </div>
-            <div id='2'>
+            <div id='7' className="hide">
                 my budget is
                 <FormControl className={this.useStyles1.formControl}>
                     <Select
                         displayEmpty
                         className={this.useStyles.selectEmpty}
                         inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangeBudget}
                     >
                         <MenuItem value="" disabled>
                             Budget
@@ -191,15 +299,31 @@ class Page3 extends Component {
                     </Select>
                 </FormControl>
             </div>
-            <div id='2'>
+            <div id='8' className="hide">
                 you can email me at
-                <TextField id="filled-basic" label="Name" variant="filled" type="email" />
+                <TextField id="filled-basic" type="email" onChange={this.handleChangeEmail} label="Name" variant="filled" type="email" />
             </div>
-            <div id='2'>
+            <div id='9' className="hide">
                 or WhatsApp me at
-                <Countries>
-                </Countries>
-                <TextField id="filled-basic" label="Name" variant="filled" type="text" />
+                <FormControl className={this.useStyles1.formControl}>
+                    <Select
+                        displayEmpty
+                        className={this.useStyles.selectEmpty}
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        onChange={this.handleChangePhoneCode}
+                    >
+                        <MenuItem value="" disabled>
+                            Budget
+                        </MenuItem>
+                        <MenuItem value={'62'}>+62</MenuItem>
+                        <MenuItem value={'73'}>+73</MenuItem>
+                        <MenuItem value={'01'} >+01</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField id="filled-basic" type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" onChange={this.handleChangeNumber} label="Name" variant="filled" type="text" />
+            </div>
+            <div id="10" className="hide" >
+                <button onClick={this.handleSubmit} ></button>
             </div>
 
 
