349e42839d9b0622ebd7557a5e3fd5184e6f267f
MhdAnggieRinovka <anggierinovka@gmail.com>
2022-12-03 12:52:24 +0700

    change printPDF

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
 const Button = (props) => {
+  return (
+    <React.Fragment>
+      {
+        <button
+          className={props.type === "fill" ? "btn-filled" : "btn-transparent"}
+          style={{ width: props.customWidth }}
+        >
+          {props.value}
+        </button>
+      }
+    </React.Fragment>
+  );
+};
 
-    return (
-        <React.Fragment>
-            {
-                <button className={props.type === "fill" ? 'btn-filled' : 'btn-transparent'} style={{width:props.customWidth}}>
-                    {props.value}
-                </button>
-            }
-        </React.Fragment>
-    )
-}
-
-export default Button
+export default Button;
